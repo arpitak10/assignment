48867d94a34c2c64a24034700ef31e317487e882
arpitak10 <99887732+arpitak10@users.noreply.github.com>
2023-08-28 12:11:10 +0530

    Update and rename java_ass_25Aug.java to java_ass_24Aug.java

--- java_ass_24Aug.java
+++ java_ass_24Aug.java
@@ -49,7 +49,7 @@ class IntegerArray {
 }
 
 public class Main {
-    public static void main(String[] args) {
+    public static void java_ass_24Aug(String[] args) {
         IntegerArray intArr = new IntegerArray(7);
         for (int i=0;i<7;i++){
             intArr.setElement(i,(i+1)*2);
